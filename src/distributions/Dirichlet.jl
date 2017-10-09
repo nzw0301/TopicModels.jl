63fa7492f36432d4de318932cf3d01db380f5dd8
Kento NOZAWA <k_nzw@klis.tsukuba.ac.jp>
2017-10-09 20:13:49 +0900

    Remove Return statement

--- src/distributions/Dirichlet.jl
+++ src/distributions/Dirichlet.jl
@@ -4,19 +4,19 @@ struct Dirichlet
     sum_alpha::Float64
 
     function Dirichlet(k::Int)
-            new(k, ones(k)/k, 1.0)
+        new(k, ones(k)/k, 1.0)
     end
 end
 
 function get_sum_alpha(dirichlet::Dirichlet)
-    return dirichlet.sum_alpha
+    dirichlet.sum_alpha
 end
 
 function get_alpha(dirichlet::Dirichlet, i::Int)
     @assert 1 <= i <= dirichlet.k
-    return dirichlet.alpha[i]
+    dirichlet.alpha[i]
 end
 
 function get_alpha_all(dirichlet::Dirichlet)
-    return dirichlet.alpha
+    dirichlet.alpha
 end
